7c7cea1810e0b0b359f854e4b40e8c42728c9d90
Jan Schwarz <jan.schwarz@strv.com>
2021-12-15 17:26:27 +0100

    Implement base tests

--- Tests/Container/ContainerBaseTests.swift
+++ Tests/Container/ContainerBaseTests.swift
@@ -11,7 +11,7 @@ import DependencyInjection
 final class ContainerBaseTests: XCTestCase {
     class Dependency {}
     
-    func testSharedAutoclosureRegistration() {
+    func testAutoclosureDependency() {
         let container = Container()
         
         let dependency = Dependency()
@@ -20,5 +20,68 @@ final class ContainerBaseTests: XCTestCase {
         let resolvedDependency: Dependency = container.resolve()
         
         XCTAssertTrue(dependency === resolvedDependency, "Container returned different instance")
+    }
+    
+    func testAutoclosureDependencyWithExplicitType() {
+        let container = Container()
+        
+        let dependency = Dependency()
+        container.register(type: Dependency.self, dependency: dependency)
+        
+        let resolvedDependency: Dependency = container.resolve()
+        
+        XCTAssertTrue(dependency === resolvedDependency, "Container returned different instance")
+    }
+
+    func testDependencyRegisteredInDefaultScope() {
+        let container = Container()
+        
+        container.register() { _ -> Dependency in
+            Dependency()
+        }
+        
+        let resolvedDependency1: Dependency = container.resolve()
+        let resolvedDependency2: Dependency = container.resolve()
+
+        XCTAssertTrue(resolvedDependency1 === resolvedDependency2, "Container returned different instance")
+    }
+
+    func testDependencyRegisteredInDefaultScopeWithExplicitType() {
+        let container = Container()
+        
+        container.register(type: Dependency.self) { _ -> Dependency in
+            Dependency()
+        }
+        
+        let resolvedDependency1: Dependency = container.resolve()
+        let resolvedDependency2: Dependency = container.resolve()
+
+        XCTAssertTrue(resolvedDependency1 === resolvedDependency2, "Container returned different instance")
+    }
+
+    func testSharedDependency() {
+        let container = Container()
+        
+        container.register(in: .shared) { _ -> Dependency in
+            Dependency()
+        }
+        
+        let resolvedDependency1: Dependency = container.resolve()
+        let resolvedDependency2: Dependency = container.resolve()
+
+        XCTAssertTrue(resolvedDependency1 === resolvedDependency2, "Container returned different instance")
+    }
+
+    func testNonSharedDependency() {
+        let container = Container()
+        
+        container.register(in: .new) { _ -> Dependency in
+            Dependency()
+        }
+        
+        let resolvedDependency1: Dependency = container.resolve()
+        let resolvedDependency2: Dependency = container.resolve()
+
+        XCTAssertTrue(resolvedDependency1 !== resolvedDependency2, "Container returned the same instance")
     }
 }
