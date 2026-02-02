1a71427f92a0f74253c0f9a9e0fc5ddf64ad406b
Daniel Čech <daniel.cech@gmail.com>
2026-02-02 17:58:07 +0100

    feat: phase 2

--- Sources/Models/Async/AsyncRegistration.swift
+++ Sources/Models/Async/AsyncRegistration.swift
@@ -22,48 +22,22 @@ struct AsyncRegistration: Sendable {
         asyncRegistrationFactory = { resolver, _ in await factory(resolver) }
     }
 
-    /// Initializer for registrations that expect a variable argument passed to the factory closure when the dependency is being resolved
-    init<Dependency: Sendable, Argument: Sendable>(type: Dependency.Type, scope: DependencyScope, factory: @Sendable @escaping (any AsyncDependencyResolving, Argument) async -> Dependency) {
-        let registrationIdentifier = RegistrationIdentifier(type: type, argumentTypes: Argument.self)
+    /// Initializer for registrations that expect variable arguments passed to the factory closure when the dependency is being resolved
+    ///
+    /// Uses Swift parameter packs to support 1-3 arguments with a single initializer.
+    init<Dependency: Sendable, each Argument: Sendable>(type: Dependency.Type, scope: DependencyScope, factory: @Sendable @escaping (any AsyncDependencyResolving, repeat each Argument) async -> Dependency) {
+        let registrationIdentifier = RegistrationIdentifier(type: type, argumentTypes: repeat (each Argument).self)
 
         identifier = registrationIdentifier
         self.scope = scope
         asyncRegistrationFactory = { resolver, arg in
-            guard let argument = arg as? Argument else {
-                throw ResolutionError.unmatchingArgumentType(message: "Registration of type \(registrationIdentifier.description) doesn't accept an argument of type \(Swift.type(of: arg))")
+            guard let arguments = arg as? (repeat each Argument) else {
+                throw ResolutionError.unmatchingArgumentType(
+                    message: "Registration of type \(registrationIdentifier.description) doesn't accept arguments of type \(Swift.type(of: arg))"
+                )
             }
 
-            return await factory(resolver, argument)
-        }
-    }
-
-    /// Initializer for registrations that expect two variable arguments passed to the factory closure when the dependency is being resolved
-    init<Dependency: Sendable, Argument1: Sendable, Argument2: Sendable>(type: Dependency.Type, scope: DependencyScope, factory: @Sendable @escaping (any AsyncDependencyResolving, Argument1, Argument2) async -> Dependency) {
-        let registrationIdentifier = RegistrationIdentifier(type: type, argumentTypes: Argument1.self, Argument2.self)
-
-        identifier = registrationIdentifier
-        self.scope = scope
-        asyncRegistrationFactory = { resolver, arg in
-            guard let arguments = arg as? (Argument1, Argument2) else {
-                throw ResolutionError.unmatchingArgumentType(message: "Registration of type \(registrationIdentifier.description) doesn't accept arguments of type \(Swift.type(of: arg))")
-            }
-
-            return await factory(resolver, arguments.0, arguments.1)
-        }
-    }
-
-    /// Initializer for registrations that expect three variable arguments passed to the factory closure when the dependency is being resolved
-    init<Dependency: Sendable, Argument1: Sendable, Argument2: Sendable, Argument3: Sendable>(type: Dependency.Type, scope: DependencyScope, factory: @Sendable @escaping (any AsyncDependencyResolving, Argument1, Argument2, Argument3) async -> Dependency) {
-        let registrationIdentifier = RegistrationIdentifier(type: type, argumentTypes: Argument1.self, Argument2.self, Argument3.self)
-
-        identifier = registrationIdentifier
-        self.scope = scope
-        asyncRegistrationFactory = { resolver, arg in
-            guard let arguments = arg as? (Argument1, Argument2, Argument3) else {
-                throw ResolutionError.unmatchingArgumentType(message: "Registration of type \(registrationIdentifier.description) doesn't accept arguments of type \(Swift.type(of: arg))")
-            }
-
-            return await factory(resolver, arguments.0, arguments.1, arguments.2)
+            return await factory(resolver, repeat each arguments)
         }
     }
 }
--- Sources/Models/Sync/Registration.swift
+++ Sources/Models/Sync/Registration.swift
@@ -20,48 +20,22 @@ struct Registration {
         self.factory = { resolver, _ in factory(resolver) }
     }
 
-    /// Initializer for registrations that expect a variable argument passed to the factory closure when the dependency is being resolved
-    init<Dependency, Argument>(type: Dependency.Type, scope: DependencyScope, factory: @escaping (DependencyResolving, Argument) -> Dependency) {
-        let registrationIdentifier = RegistrationIdentifier(type: type, argumentTypes: Argument.self)
+    /// Initializer for registrations that expect variable arguments passed to the factory closure when the dependency is being resolved
+    ///
+    /// Uses Swift parameter packs to support 1-3 arguments with a single initializer.
+    init<Dependency, each Argument>(type: Dependency.Type, scope: DependencyScope, factory: @escaping (DependencyResolving, repeat each Argument) -> Dependency) {
+        let registrationIdentifier = RegistrationIdentifier(type: type, argumentTypes: repeat (each Argument).self)
 
         identifier = registrationIdentifier
         self.scope = scope
         self.factory = { resolver, arg in
-            guard let argument = arg as? Argument else {
-                throw ResolutionError.unmatchingArgumentType(message: "Registration of type \(registrationIdentifier.description) doesn't accept an argument of type \(Swift.type(of: arg))")
+            guard let arguments = arg as? (repeat each Argument) else {
+                throw ResolutionError.unmatchingArgumentType(
+                    message: "Registration of type \(registrationIdentifier.description) doesn't accept arguments of type \(Swift.type(of: arg))"
+                )
             }
 
-            return factory(resolver, argument)
-        }
-    }
-
-    /// Initializer for registrations that expect two variable arguments passed to the factory closure when the dependency is being resolved
-    init<Dependency, Argument1, Argument2>(type: Dependency.Type, scope: DependencyScope, factory: @escaping (DependencyResolving, Argument1, Argument2) -> Dependency) {
-        let registrationIdentifier = RegistrationIdentifier(type: type, argumentTypes: Argument1.self, Argument2.self)
-
-        identifier = registrationIdentifier
-        self.scope = scope
-        self.factory = { resolver, arg in
-            guard let arguments = arg as? (Argument1, Argument2) else {
-                throw ResolutionError.unmatchingArgumentType(message: "Registration of type \(registrationIdentifier.description) doesn't accept arguments of type \(Swift.type(of: arg))")
-            }
-
-            return factory(resolver, arguments.0, arguments.1)
-        }
-    }
-
-    /// Initializer for registrations that expect three variable arguments passed to the factory closure when the dependency is being resolved
-    init<Dependency, Argument1, Argument2, Argument3>(type: Dependency.Type, scope: DependencyScope, factory: @escaping (DependencyResolving, Argument1, Argument2, Argument3) -> Dependency) {
-        let registrationIdentifier = RegistrationIdentifier(type: type, argumentTypes: Argument1.self, Argument2.self, Argument3.self)
-
-        identifier = registrationIdentifier
-        self.scope = scope
-        self.factory = { resolver, arg in
-            guard let arguments = arg as? (Argument1, Argument2, Argument3) else {
-                throw ResolutionError.unmatchingArgumentType(message: "Registration of type \(registrationIdentifier.description) doesn't accept arguments of type \(Swift.type(of: arg))")
-            }
-
-            return factory(resolver, arguments.0, arguments.1, arguments.2)
+            return factory(resolver, repeat each arguments)
         }
     }
 }
